2c43d2ab11efb9a1e556555a13577e74a72327d8
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-10-17 18:13:14 +0530

    last commit updated

--- session1/example3/index.js
+++ session1/example3/index.js
@@ -1,4 +1,5 @@
 var chalk = require('chalk');
+var error = chalk.bold.red;
 
 console.log(chalk.green("this is a green color text"));
 
@@ -7,3 +8,9 @@ console.log(chalk.blue("this is a blue color text"));
 console.log(chalk.red("this is a red color text"));
 
 console.log(chalk.red.bgYellow.bold("this is text in red color with yellow background and bold style"));
+
+console.log(error("Error!"));
+
+var theme1 = chalk.blue.bgRed.bold;
+
+console.log(theme1("This written in  theme1"));
