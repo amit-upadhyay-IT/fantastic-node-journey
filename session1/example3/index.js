d682bb5eadb53efde0ce49476b31009066b4c100
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-10-17 18:07:40 +0530

    chalk example added

---
+++ session1/example3/index.js
@@ -0,0 +1,9 @@
+var chalk = require('chalk');
+
+console.log(chalk.green("this is a green color text"));
+
+console.log(chalk.blue("this is a blue color text"));
+
+console.log(chalk.red("this is a red color text"));
+
+console.log(chalk.red.bgYellow.bold("this is text in red color with yellow background and bold style"));
