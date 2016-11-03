1a2b723dc1610a17e7a8202aa745976666c35ef4
Amit Upadhyay <youarefreak1@gmail.com>
2016-11-03 19:24:51 +0530

    Update add2.js

--- session1/example1/add2.js
+++ session1/example1/add2.js
@@ -4,3 +4,9 @@ function add(x, y, z)
 }
 
 module.exports = add;
+
+/*
+Here we are directly exporting the defination of the method add, so here if I would have written 
+module.exports.add = add;
+then inside the index.js i need to write add2.add(); while calling the above method
+*/
