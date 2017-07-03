cf409f7598c65e012176b7443ec2328244a3196b
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-07-03 17:44:56 +0530

    obj props print

--- session1/objects_example/examplefile.js
+++ session1/objects_example/examplefile.js
@@ -4,3 +4,9 @@ obj.age = "20";
 obj.rank = "001";
 
 console.log(Object.keys(obj));
+
+var props = Object.keys(obj);
+for (var i = 0; i < props.length; ++i)
+{
+    console.log(props[i] + ' : '+ obj[props[i]]);
+}
