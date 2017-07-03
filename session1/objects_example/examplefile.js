5b485f4703892dd484e1bbfe9774fe2e9e8d1c09
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-07-03 18:04:27 +0530

    constructors in js

--- session1/objects_example/examplefile.js
+++ session1/objects_example/examplefile.js
@@ -5,8 +5,21 @@ obj.rank = "001";
 
 console.log(Object.keys(obj));
 
+// iterating through the object obj.
 var props = Object.keys(obj);
 for (var i = 0; i < props.length; ++i)
 {
     console.log(props[i] + ' : '+ obj[props[i]]);
 }
+
+// constructors
+function TreeHouse(name, tree, color)
+{
+     this.housename = name;
+     this.treename = tree;
+     this.housecolor = color;
+}
+
+TreeHouse.prototype.owner = 'Amit Upadhyay';
+var amitTreeHouse = new TreeHouse('Babu', 'Oak', 'Yellow');
+console.log(amitTreeHouse.owner);
