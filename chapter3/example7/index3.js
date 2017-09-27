0bcc2f5d9a1dc3e0fe93a725bf117d66b8342668
amit-upadhyay-it <youarefreak1@gmail.com>
2017-09-27 18:07:30 +0530

    cds

--- chapter3/example7/index3.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var zlib = require('zlib');
-
-var readableStream = fs.createReadStream('./bower_components/angular/angular.min.js.gzip');// reading the gzip file
-
-readableStream
-    .pipe(zlib.createGunzip())// piping to gzip stream
-    .pipe(fs.createWriteStream('angular.min.js'));// finally piping to output file
