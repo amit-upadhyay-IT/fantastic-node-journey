4a7cece75deb691354ebc9a0727b8461c361f984
amit-upadhyay-it <youarefreak1@gmail.com>
2017-09-27 18:06:35 +0530

    cd

---
+++ chapter3/example7:streams/index.js
@@ -0,0 +1,28 @@
+/*
+ * example of read stream
+ * */
+
+var fs = require('fs');
+
+var stream = fs.createReadStream('data.large.txt');
+
+stream.setEncoding('UTF-8');// to make the stream readable
+
+var chunks = 0;
+
+stream.on('data', function(data) {// instead of loading it with chunk by chunk we could have directly used pipe(), but this example is giving more better view of streams
+    console.log('chunk', data);
+    console.log('loaded part of the file');
+    console.log('************************************************');
+    console.log('size of each chunk is %d bytes ', data.length);
+    chunks++;// calculating in how many chunks we get the complete data of the stream
+});
+
+stream.on('end', function() {
+    console.log('all parts are loaded in ', chunks, 'chunk(s)');
+});
+
+stream.on('error', function(err) {
+    console.log('Something is wrong :( ');
+});
+
