60586993811f8f33b8ef11255212e08990e18093
amit-upadhyay-it <youarefreak1@gmail.com>
2017-10-11 21:22:05 +0530

    custom async function

--- chapter2/example4_making_a_callback/index.js
+++ chapter2/example4_making_a_callback/index.js
@@ -1,6 +1,9 @@
 
-setTimeout (function (){
-    console.log('World');
-}, 1000);
-
-console.log('Hello');
+function printWorld()
+{
+    setTimeout (function (){
+        console.log('World');
+    }, 1000);
+}
+printWorld();
+console.log('Hello'); // this gets printed first because the above function is pushed into the event queue (not onto the call stack)
