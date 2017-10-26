16ddce1ec616811e4615110dd108d99e3ef2276f
amit-upadhyay-it <youarefreak1@gmail.com>
2017-10-26 11:34:04 +0530

    saving data in mongodb via mongoose

--- chapter5/example3_mongoose/index.js
+++ chapter5/example3_mongoose/index.js
@@ -11,3 +11,21 @@ db.once('open', function (callback) {
     console.log('Successfully connected to MongoDB');
 });
 
+
+// creating a new schema
+var kittySchema = mongoose.Schema({
+    name: String// in this example I had only one property in this schema but we can have more for some other examples.
+});
+
+// using the schema above we need to create the model
+var Kitten = mongoose.model('Kitten', kittySchema); // the second arg is the schema name.
+
+// now we create a new cat (a document in mongodb)
+var silence = new Kitten({name: 'Silence'});
+
+// this save method is going to save the data in the database.
+silence.save(function (err) {
+    if (err)
+        console.log(err);
+    console.log('meow');
+});
